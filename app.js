0e02e6e070cdc3c28f2071202376593f28af3821
Joker <907049175@qq.com>
2019-01-19 17:38:49 +0800

    app.js finish

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 var express = require('express');
 var app = express();
 var router = require('./controller/router.js');
+var session = require('express-session');
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+app.use('/avatar', express.static('./avatar'));
+
+app.get('/', router.showIndex);
+app.get('/regist', router.showRegist);
+app.post('/doRegist', router.doRegist);
+app.get('/login', router.showLogin);
+app.post('doLogin', router.doLogin);
+app.get('/cut', router.showCut);
+app.post('doCut', router.doCut);
+app.post('/setAvatar', router.setAvatar);
+app.get('/user/:user', router.showUser);
+app.get('/userList', router.showUserList);
+app.post('/post', router.doPost);
+app.get('/getAllTalk', router.getAllTalk);
+app.get('/getUserInfo', router.getUserInfo);
+app.get('/getTalkAmount', router.getTalkAmount);
+
+app.listen(3000);
+
